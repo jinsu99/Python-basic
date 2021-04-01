b2f9e5910697c352a63e510ad0e5fa5d6d9a7d5f
jinsukuku <jinsukuku72@gmail.com>
2021-04-01 19:52:47 +0900

    practice02 hint

--- practice02/02-1.py
+++ practice02/02-1.py
@@ -1,3 +1,9 @@
-# 문제1. 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요.
+# 문제1-1. 파이썬 경로명 s = '/usr/local/bin/python' 에서 각각의 디렉토리 경로명을 분리하여 출력하세요.
 
 s = '/usr/local/bin/python'
+s = s[1:len(s)]
+str = s.split('/')
+for s in str :
+    print("'{0}'".format(s), end=' ')
+
+# 문제1-2. 디렉토리 경로명과 파일명을 분리하여 출력하세요.
